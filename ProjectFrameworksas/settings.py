d6e53676e272db8e6793c943bfa6e0a90dc7618e
OsvaldoAguilar2024 <Oaguilar20212021@gmail.com>
2025-04-30 10:48:49 -0500

    version test 2

--- ProjectFrameworksas/settings.py
+++ ProjectFrameworksas/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 import environ
@@ -18,10 +19,13 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-#SECRET_KEY = env("DJANGO_SECRET_KEY")
-#DEBUG = env("DJANGO_DEBUG")
-SECRET_KEY = "django-insecure-&&!qmb&f85=uyc7!_ize!lb&!q$@)d0nc0)im_31$in@x*v7r^"
-DEBUG = False
+
+
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+DEBUG = env("DJANGO_DEBUG")
+#SECRET_KEY = "django-insecure-&&!qmb&f85=uyc7!_ize!lb&!q$@)d0nc0)im_31$in@x*v7r^"
+#DEBUG = False
 
 
 
@@ -34,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-ALLOWED_HOSTS = ["*" ,"presupuestador.up.railway.app"]
+ALLOWED_HOSTS = ["*" ,"*.up.railway.app"]
 
 
 # Application definition
@@ -58,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -102,12 +107,29 @@ WSGI_APPLICATION = "ProjectFrameworksas.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "dbframe3.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ZCMMFSTVUVdVsKNFhOfyoEbNEzZQzJjE' ,
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432'
     }
 }
+
+
+
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "dbframe3.sqlite3",
+#    }
+#}
 
 
 # Password validation
@@ -148,7 +170,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # New line
 MEDIA_URL = '/media/'
