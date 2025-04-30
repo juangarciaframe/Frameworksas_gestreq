58a42d96e99d147fadb2663827c7c55ce45a614a
OsvaldoAguilar2024 <Oaguilar20212021@gmail.com>
2025-04-30 11:28:15 -0500

    version test 7

--- ProjectFrameworksas/settings.py
+++ ProjectFrameworksas/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # New line
