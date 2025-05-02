20ce27c7c92c076996cf5a55cbd91d890245ac45
OsvaldoAguilar2024 <Oaguilar20212021@gmail.com>
2025-05-01 19:33:46 -0500

    prod008

--- ProjectFrameworksas/settings.py
+++ ProjectFrameworksas/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # Comentado si no tienes una carpeta static a nivel de proyecto
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # New line
