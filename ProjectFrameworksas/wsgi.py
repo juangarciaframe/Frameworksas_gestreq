c0e492895f2194f49449cfaeebf83d29cc9d8d08
OsvaldoAguilar2024 <Oaguilar20212021@gmail.com>
2025-05-01 19:47:34 -0500

    FINAL TEST 002

--- ProjectFrameworksas/wsgi.py
+++ ProjectFrameworksas/wsgi.py
@@ -16,6 +16,10 @@ from django.conf import settings # Importar settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProjectFrameworksas.settings")
 
 application = get_wsgi_application()
+
+# Asegurarse de que MEDIA_ROOT exista antes de que WhiteNoise lo use
+os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+
 # Añadir WhiteNoise para servir media (NO RECOMENDADO PARA PRODUCCIÓN)
 application = WhiteNoise(application, root=settings.MEDIA_ROOT)
 application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
